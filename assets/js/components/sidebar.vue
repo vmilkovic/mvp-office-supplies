9e954b242e57b74dbdb858b9840d430768897b98
Vedran Milković <vedran.milkovic25@gmail.com>
2021-04-18 20:46:09 +0200

    Loops, reactivity and computed properties

--- assets/js/components/sidebar.vue
+++ assets/js/components/sidebar.vue
@@ -1,43 +1,93 @@
 <template>
-    <div :class="[$style.sidebar, 'p-3', 'mb-5']">
-        <h5 class="text-center">
-            Categories
-        </h5>
+    <div
+        :class="componentClass"
+    >
+        <div v-show="!collapsed">
+            <h5 class="text-center">
+                Categories
+            </h5>
 
-        <ul class="nav flex-column mb4">
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="/"
-                >All Products</a>
-            </li>
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="#"
-                >Category A</a>
-            </li>
-            <li class="nav-item">
-                <a
-                    class="nav-link"
-                    href="#"
-                >Category B</a>
-            </li>
-        </ul>
+            <ul class="nav flex-column mb4">
+                <li class="nav-item">
+                    <a
+                        class="nav-link"
+                        href="/"
+                    >All Products</a>
+                </li>
+                <li
+                    v-for="(category, index) in categories"
+                    :key="index"
+                    class="nav-item"
+                >
+                    <a
+                        class="nav-link"
+                        :href="category.link"
+                    >{{ category.name }}</a>
+                </li>
+            </ul>
+
+            <hr>
+        </div>
+        <div class="d-flex justify-content-end">
+            <button
+                class="btn btn-secondary btn-sm"
+                @click="toggleCollapsed"
+                v-text="collapsed ? '>>' : '<< Collapse'"
+            />
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'Sidebar',
+    data() {
+        return {
+            collapsed: false,
+            categories: [
+                {
+                    name: 'Dot Matrix Printers',
+                    link: '#',
+                },
+                {
+                    name: 'Iomega Zip Drives',
+                    link: '#',
+                },
+            ],
+        };
+    },
+    computed: {
+        /**
+         * Computes the component classes depending on collapsed state
+         * @returns string[]
+         */
+        componentClass() {
+            const classes = [this.$style.component, 'p-3', 'mb-5'];
+
+            if (this.collapsed) {
+                classes.push(this.$style.collapsed);
+            }
+
+            return classes;
+        },
+    },
+    methods: {
+        toggleCollapsed() {
+            this.collapsed = !this.collapsed;
+        },
+    },
 };
 </script>
 
 <style lang="scss" module>
 @import '~styles/components/light-component';
 
-.sidebar {
+.component {
     @include light-component;
+
+    &.collapsed {
+        width: 70px;
+    }
 
     ul {
         li a:hover {
